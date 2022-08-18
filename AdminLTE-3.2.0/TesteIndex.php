4b4e392d6c389cdb36f63b7a9481960d82ffd82a
OctavioMoa <octavioaugusto098@gmail.com>
2022-08-18 12:17:00 -0300

    Muita coisa

--- AdminLTE-3.2.0/TesteIndex.php
+++ AdminLTE-3.2.0/TesteIndex.php
@@ -879,16 +879,15 @@
 
     <!-- Main content -->
     <section class="content">
-      <div class="container-fluid">
         <!-- Small boxes (Stat box) -->
         <div class="row">
           <div class="col-lg-3 col-6">
             <!-- small box -->
             <div class="small-box bg-info">
               <div class="inner">
-                <h3>Welcome</h3>
+                <h3><?php echo $f['username'];?></h3>
 
-                <p><?php echo $f['username'];?></p>
+                <p>Welcome</p>
               </div>
               <div class="icon">
                 <i class="ion ion-bag"></i>
